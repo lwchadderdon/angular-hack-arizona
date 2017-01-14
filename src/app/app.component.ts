d033d22a26f48f4fa26a01334a9e67f126da4258
lwchadderdon <lwchadderdon@gmail.com>
2017-01-14 16:52:22 -0700

    Show community favorites from Firebase.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {BooksService, Book} from "./books.service";
 import {Observable} from "rxjs";
+import {AngularFire, FirebaseListObservable} from "angularfire2";
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,10 @@ import {Observable} from "rxjs";
 export class AppComponent {
   title: string = 'Angular2 - HackArizona';
   books: Observable<Book[]>;
+  favorites: FirebaseListObservable<any[]>;
 
-  constructor(private booksService: BooksService) {
+  constructor(private booksService: BooksService, private af: AngularFire) {
+    this.favorites = af.database.list("/favorites");
   }
 
   search(query: string) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {BooksService} from "./books.service";
+import {AngularFireModule, FirebaseAppConfig} from "angularfire2";
+
+export const firebaseConfig: FirebaseAppConfig = {
+  apiKey: 'AIzaSyDIsA68YcrULjJ6XSBkeXNlvKGHMbM8Ej8',
+  authDomain: 'nifty-field-101400.firebaseapp.com',
+  databaseURL: 'https://nifty-field-101400.firebaseio.com',
+  storageBucket: 'gs://nifty-field-101400.appspot.com',
+  messagingSenderId: '881330966449'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
     BrowserModule,
     FormsModule,
     HttpModule
@@ -18,4 +28,5 @@ import {BooksService} from "./books.service";
   providers: [BooksService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
